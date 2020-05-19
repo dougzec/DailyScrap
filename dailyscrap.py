7fef5f1de55098a87a7101d01c972bf91f59efb0
dougzec <51807148+dougzec@users.noreply.github.com>
2020-05-19 12:04:22 -0300

    Update dailyscrap.py

--- dailyscrap.py
+++ dailyscrap.py
@@ -12,18 +12,22 @@ chromeOptions = webdriver.ChromeOptions()
 prefs = {'profile.managed_default_content_settings.images':2}
 chromeOptions.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(options=chromeOptions)
+driver.minimize_window()
 
-# Read searches.json. This file contains user informed functions and parameters to call
-with open('searches.json') as f:
-    data = json.load(f)
+try:
+    # Read searches.json. This file contains user informed functions and parameters to call
+    with open('searches.json') as f:
+        data = json.load(f)
 
-# Run web scrap!
-for key in list(data): # Iterates over searches.json keys
-    try:
-        inputs = data[key]
-        locals()[key](inputs, driver) # activates and runs a function with the same name as the key and uses the values as input
-    except:
-        print("Feature '{}' not implemented") # in case something goes wrong...
-        
-# Closes the Driver
-driver.close()
+    # Run web scrap!
+    for key in list(data): # Iterates over searches.json keys
+        try:
+            inputs = data[key]
+            locals()[key](driver, inputs) # activates and runs a function with the same name as the key and uses the values as input
+        except:
+            print("Feature '{}' not implemented") # in case something goes wrong...
+    # Closes the Driver
+    driver.close()
+except:
+    # Closes the Driver
+    driver.close()
